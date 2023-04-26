7fe7937f8b99f8a3ba9110d4b2462d366aeccdba
Gwangnam97 <107448135+Gwangnam97@users.noreply.github.com>
2023-04-26 09:20:19 +0900

    .

--- kakao/application.py
+++ kakao/application.py
@@ -5,12 +5,19 @@ from fastapi import FastAPI, Request
 
 
 # 컨텍스트 생성하기 (기쁨->슬픔)
-# 퀵 리플 만들기
 
 # MySQL connection configuration
-config = {"host": "localhost", "user": "root", "password": "1234", "database": "sys"}
+config = {"host": "localhost", "user": "root",
+          "password": "1234", "database": "sys"}
 
 app = FastAPI()
+
+block_id_send_img_random = '64415440013416338a0853a3'
+block_id_send_img = '64477498d853bb56940a87bd'
+
+quick_replies_labels = ["기쁨", "슬픔", "분노", "황당", "놀람", "졸림"]
+quick_replies = [{"label": label, "action": "block",
+                  "blockId": block_id_send_img} for label in quick_replies_labels]
 
 
 @app.post("/recommend")
@@ -19,7 +26,7 @@ async def recommend(request: Request):
         "version": "2.0",
         "template": {
             "outputs": [
-                {"simpleText": {"text": "✨추천 도와드릴게요!\n☝우선 어떻게 추천해줄지 정해볼까요?"}},
+                {"simpleText": {"text": "✨추천 도와드릴게요!\n☝우선 어떻케 추천해줄지 정해볼까요?"}},
                 {
                     "carousel": {
                         "type": "basicCard",
@@ -44,9 +51,11 @@ async def recommend(request: Request):
                                 },
                                 "buttons": [
                                     {
-                                        "action": "message",
-                                        "label": "밈모에게 얘기하기",
-                                        "messageText": "밈모에게 얘기하기",
+                                        "label": "공유하기",
+                                        "action":  "share",
+                                        # "action": "message",
+                                        # "label": "밈모에게 얘기하기",
+                                        # "messageText": "밈모에게 얘기하기",
                                     }
                                 ],
                             },
@@ -62,9 +71,9 @@ async def recommend(request: Request):
 
 async def get_category_name(req: dict):
     try:
-        return req["action"]["clientExtra"]["name"]
+        return req["userRequest"]["utterance"]
     except KeyError:
-        return "None"
+        return "Read_Error!!!"
 
 
 async def get_image_data(category_name: str):
@@ -89,11 +98,43 @@ async def get_image_data(category_name: str):
     return items
 
 
+async def get_image_random_data(category_name: str):
+    # query = f"SELECT url FROM sys.pinterest where crawled_at like ('%{category_name}%') ORDER BY RAND() LIMIT 3;"
+    query = "SELECT url FROM sys.pinterest where crawled_at like ('%짤방%') ORDER BY RAND() LIMIT 3;"
+
+    with mysql.connector.connect(**config) as conn:
+        cursor = conn.cursor()
+        cursor.execute(query)
+        results = cursor.fetchall()
+
+    items = [
+        {
+            "title": f"listCard 테스트#{index + 1}",
+            "description": f"listCard 테스트#{index + 1} description",
+            "thumbnail": {
+                "imageUrl": result[0],
+                "link": {"web": result[0]},
+            },
+            "buttons": [
+                {
+                    "label": "공유하기",
+                    "action":  "share",
+                    # "webLinkUrl": result[0]
+                }
+            ]
+        }
+        for index, result in enumerate(results)
+    ]
+
+    return items
+
+
 @app.post("/send_img")
 async def send_img(request: Request):
     req = await request.json()
+    print(req)
     category_name = await get_category_name(req)
-    block_id = "64415440013416338a0853a3"
+    block_id = "64477498d853bb56940a87bd"
     items = await get_image_data(category_name)
 
     res = {
@@ -109,7 +150,6 @@ async def send_img(request: Request):
                                 "action": "block",
                                 "label": "더보기",
                                 "blockId": block_id,
-                                "extra": {"name": category_name},
                             }
                         ],
                     }
@@ -120,7 +160,9 @@ async def send_img(request: Request):
                         "items": items,
                     }
                 },
-            ]
+            ],
+            "quickReplies": quick_replies,
+
         },
     }
 
@@ -130,9 +172,9 @@ async def send_img(request: Request):
 @app.post("/send_img_random")
 async def send_img_random(request: Request):
     req = await request.json()
-    block_id = "64415440013416338a0853a3"
-    category_name = "tmp"
-    items = await get_image_data(category_name)
+    print(req)
+    category_name = await get_category_name(req)
+    items = await get_image_random_data(category_name)
 
     res = {
         "version": "2.0",
@@ -140,27 +182,36 @@ async def send_img_random(request: Request):
             "outputs": [
                 {
                     "basicCard": {
-                        "title": "무작위의 밈을 추천해드립니다. " "더 많은 밈을 보고싶은 경우 아래의 더보기를 눌러주세요",
+                        "title": f"{category_name}의 밈을 추천해드립니다. " "더 많은 밈을 보고싶은 경우 아래의 더보기를 눌러주세요",
                         "buttons": [
                             {
                                 "action": "block",
                                 "label": "더보기",
-                                "blockId": block_id,
-                                "extra": {"name": category_name},
+                                "blockId": block_id_send_img_random,
                             }
                         ],
                     }
                 },
                 {
-                    "listCard": {
-                        "header": {"title": "listCard 테스트"},
+                    "carousel": {
+                        "type": "basicCard",
                         "items": items,
                     }
                 },
-            ]
+            ],
+            "quickReplies": quick_replies,
         },
+
     }
 
+    return JSONResponse(content=res)
+
+
+@app.post("/talk_to_mememo")
+async def talk_to_mememo(request: Request):
+    req = await request.json()
+
+    res = "tmp"
     return JSONResponse(content=res)
 
 
